428998027daef08d29837244a207b5b68765fed4
Daniela <danielavieira2024@gmail.com>
2022-09-17 17:13:54 -0300

    first commit

---
+++ 01/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Desenvolvendo meu primeiro Projeto!</h1>
+      <h2>Estou empolgada para concluir esse curso.</h2>
+      <p>Vamos assistir as aulas</p>
+    </div>
+  );
+}
+
+
+export default App;
